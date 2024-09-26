b15e4481dcd8a1909f592b67f33b857cc5511251
Tynwald <125758095+Tynwaldd@users.noreply.github.com>
2024-09-26 11:03:15 +0300

    Create PR_1.py

---
+++ PR_1.py
@@ -0,0 +1,8 @@
+last_name = input("Ваша фамилия? ")
+name = input("Ваше имя? ")
+year = input("Сколько Вам лет? ")
+place = input("Где вы живете? ")
+
+print("Ваши фамилия, имя: ", last_name, name)
+print("Ваш возраст: ", year)
+print("Вы живете в: ", place)
