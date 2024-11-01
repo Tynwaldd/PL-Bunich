578e6bfa8cd090c993575efaf4d155f556d819b1
Tynwald <125758095+Tynwaldd@users.noreply.github.com>
2024-11-01 12:27:18 +0300

    Create PR_7_option_3.py

---
+++ PR_7_option_3.py
@@ -0,0 +1,14 @@
+#1
+dlina = int(input())
+D = []
+for n in range(dlina):
+        D.append(n)
+res = sum([int(i) for i in range(len(D) + 1) if i % 2 != 0])
+print(res)
+
+#2
+array = [1, 23, 4, 7, 8, 54, 14, 4]
+for i in range(len(array)):
+        if array[i] < 15:
+                array[i] *= 2
+print(array)
